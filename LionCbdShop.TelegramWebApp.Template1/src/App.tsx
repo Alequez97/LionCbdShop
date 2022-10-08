1719f6e4ba8e1329ab1e07feddbf8460be75427f
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-10-08 09:34:11 +0300

    Implement web app custom main button

--- LionCbdShop.TelegramWebApp.Template1/src/App.tsx
+++ LionCbdShop.TelegramWebApp.Template1/src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
-import Cart from "./components/Cart/Cart"
 import Loader from './components/Loader/Loader';
 import Error from './components/Error/Error';
 import { useProductsMock } from './hooks/productsMock';
@@ -9,48 +8,32 @@ import { getCartItemsAsJsonString } from './helpers'
 import { addProduct, removeProduct } from './store/cartItems';
 import { useAppDispatch } from './store/store';
 import { useCartItems } from './hooks/cartItems';
+import Footer from './components/Footer/Footer';
 
 const telegramWebApp = window.Telegram.WebApp;
 
 function App() {
   useEffect(() => {
     telegramWebApp.ready();
-    // telegramWebApp.onEvent('mainButtonClicked', () => {
-    //   const json = getCartItemsAsJsonString(cartItems);
-    //   telegramWebApp.sendData(json);
-    // })
+    telegramWebApp.expand();
   }, []);
 
-  const { cartItems } = useCartItems();
-  useEffect(() => { 
-    cartItems.length === 0 ? telegramWebApp.MainButton.hide() : telegramWebApp.MainButton.show()
-  }, [cartItems]);
-
   const dispatch = useAppDispatch();
+  const { cartItems } = useCartItems();
   const { products, error, loading } = useProductsMock();
 
-  let sendDataToTelegramWebApp = () => {
+  function sendDataToTelegramWebApp() {
+    console.log('send data');
     const json = getCartItemsAsJsonString(cartItems);
     telegramWebApp.sendData(json);
   }
 
   const onAdd = (productId: string) => {
     dispatch(addProduct(productId))
-
-    telegramWebApp.MainButton.offClick(sendDataToTelegramWebApp);
-    telegramWebApp.MainButton.onClick(sendDataToTelegramWebApp);
   };
 
   const onRemove = (productId: string) => {
     dispatch(removeProduct(productId))
-
-    telegramWebApp.MainButton.offClick(sendDataToTelegramWebApp);
-    telegramWebApp.MainButton.onClick(sendDataToTelegramWebApp);
-  };
-
-  const onCheckout = () => {
-    // const json = getCartItemsAsJsonString(cartItems);
-    // telegramWebApp.sendData(json);
   };
 
   if (loading) {
@@ -69,17 +52,15 @@ function App() {
     <>
       <h2 className="heading">Royal MMXXI</h2>
 
-      <p>Cart items:</p>
-      <br />
-      {JSON.stringify(cartItems)}
-
-      {/* <Cart cartItems={cartItems} onCheckout={onCheckout} /> */}
-
       <div className="cards__container">
         {products.map(product => <Card product={product} key={product.id} onAdd={onAdd} onRemove={onRemove} />)}
       </div>
 
-      {/* <TelegramMainButton /> */}
+      {<Footer 
+        mainButtonText={'Checkout'}
+        mainButtonOnClick={sendDataToTelegramWebApp} 
+        visible={cartItems.length !== 0} 
+      />}
     </>
   );
 }
