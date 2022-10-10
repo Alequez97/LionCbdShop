cf820f9c78c8c9d02bb198524e7628f523b66831
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-10-10 18:45:43 +0300

    Setup admin react client app from scratch

--- LionCbdShop.Admin/ClientApp/webpack/webpack.analyze.ts
+++
@@ -1,9 +0,0 @@
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import StatoscopeWebpackPlugin from '@statoscope/webpack-plugin';
-
-export default {
-  plugins: [
-    new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
-    new StatoscopeWebpackPlugin(),
-  ],
-};
