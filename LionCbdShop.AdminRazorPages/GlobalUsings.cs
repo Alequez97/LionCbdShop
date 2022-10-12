7bf2e56085591e6096b5682110e31127cf8ead67
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-10-13 00:15:00 +0300

    delete razor pages admin project

--- LionCbdShop.AdminRazorPages/GlobalUsings.cs
+++
@@ -1,2 +0,0 @@
-﻿global using GetProductsResponse = LionCbdShop.Domain.Response<System.Collections.Generic.IEnumerable<LionCbdShop.Domain.Dto.ProductDto>>;
-global using DomainLayerResponse = LionCbdShop.Domain.Response;
