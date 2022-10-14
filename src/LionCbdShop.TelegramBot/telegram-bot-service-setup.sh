de8b13cd8d9e8b186e75d1eac0b21afddcb47277
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-10-15 02:05:55 +0300

    Add bash script that setups systemd service

---
+++ src/LionCbdShop.TelegramBot/telegram-bot-service-setup.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+mkdir /srv/LionCbdShop.TelegramBot
+chown root /srv/LionCbdShop.TelegramBot
+dotnet publish -c Release -o /srv/LionCbdShop.TelegramBot
+
+sudo cp TelegramBot.service /etc/systemd/system/TelegramBot.service
+sudo systemctl daemon-reload
+sudo systemctl start TelegramBot
+sudo systemctl status TelegramBot
