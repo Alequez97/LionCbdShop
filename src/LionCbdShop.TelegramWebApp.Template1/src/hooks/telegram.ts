c4f74d7a2102f20f388ad24a81f58a591a8d55ad
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-10-14 18:23:16 +0300

    Remove unused function in telegram hook

--- src/LionCbdShop.TelegramWebApp.Template1/src/hooks/telegram.ts
+++ src/LionCbdShop.TelegramWebApp.Template1/src/hooks/telegram.ts
@@ -3,10 +3,6 @@ const telegramWebApp = window.Telegram.WebApp
 export function useTelegramWebApp() {
     telegramWebApp.ready();
 
-    const showButton = () => {
-        
-    }
-
     return {
         telegramWebApp
     }
