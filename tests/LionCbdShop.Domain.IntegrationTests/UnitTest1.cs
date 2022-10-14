b4cc3929a1e505993b317e9d107ca3e0b3f9991e
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-10-14 16:48:18 +0300

    Reformat project structure and add blank tests project

---
+++ tests/LionCbdShop.Domain.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace LionCbdShop.Domain.IntegrationTests;
+
+public class ProductServiceTests
+{
+    [Fact]
+    public void GetAllProducts_ResponseShouldBeSuccess()
+    {
+        Assert.True(true);
+    }
+}
