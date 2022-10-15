cb7f0625cbe2d2f1df2f4a9a2be432321b260d4a
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-10-16 01:32:25 +0300

    Update admin home page title

--- src/LionCbdShop.Admin/ClientApp/src/pages/Home.tsx
+++ src/LionCbdShop.Admin/ClientApp/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
         <div className="text-center">
-            <h1 className="display-4">Lion CBD Admin - Test</h1>
+            <h1 className="display-4">Lion CBD Admin</h1>
         </div>
     )
 }
