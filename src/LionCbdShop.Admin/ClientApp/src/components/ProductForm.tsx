fff74bde91c64b9efb44d639cd8729af9cdcee94
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-10-19 18:52:18 +0300

    Update product form to read values using useRef hook

--- src/LionCbdShop.Admin/ClientApp/src/components/ProductForm.tsx
+++ src/LionCbdShop.Admin/ClientApp/src/components/ProductForm.tsx
@@ -13,53 +13,47 @@ export default function ProductForm({ product }: ProductFormProps) {
     const productNameRef = useRef<HTMLInputElement | null>(null);
     const originalPriceRef = useRef<HTMLInputElement | null>(null);
     const priceWithDiscountRef = useRef<HTMLInputElement | null>(null);
-    const productFormRef = useRef<HTMLFormElement | null>(null);
+    const productImageRef = useRef<HTMLInputElement | null>(null);
 
     const navigate = useNavigate();
 
-    const [inputData, setInputData] = useState<any>({})
-    const [inputImage, setInputImage] = useState<File>()
     const [inputDataErrors, setInputDataErrors] = useState<any>({})
     const [isSubmit, setIsSubmit] = useState(false)
-
-    useEffect(() => {
-        if (product) {
-            setInputData({
-                productName: product.productName,
-                originalPrice: product.originalPrice,
-                priceWithDiscount: product.priceWithDiscount
-            })
-        }
-    }, [product])
 
     const [showInfoBadge, setShowInfoBadge] = useState(false)
     const [infoBadgeText, setShowInfoBadgeText] = useState('')
     const [infoBadgeType, setInfoBadgeType] = useState('')
 
     function validateForm() {
-        console.log('ref data:')
-        console.log(productNameRef.current?.value)
-
         let isUpdateOperation = product !== undefined;
-        
+
         let errors: any = {};
 
-        if (!inputData?.productName) {
+        let productName = productNameRef.current?.value
+        if (!productName) {
             errors.productNameError = "Product name is required"
         }
 
-        if (!inputData?.originalPrice) {
+        let originalPrice = originalPriceRef.current?.value;
+        if (!originalPrice) {
             errors.originalPriceError = "Original price is required"
-        } else if (inputData?.originalPrice <= 0) {
+        } else if (Number(originalPrice) <= 0) {
             errors.originalPriceError = "Original price should be valid number and be more than 0"
         }
 
-        if (inputData?.priceWithDiscount >= inputData?.originalPrice) {
-            errors.priceWithDiscountError = "Price with discount should be less than original price"
-        } else if (inputData?.priceWithDiscount < 0) {
-            errors.priceWithDiscountError = "Price with discount shouldn't be negative number"
-        } 
+        let priceWithDiscount = priceWithDiscountRef.current?.value;
+        console.log(priceWithDiscount)
+        console.log(originalPrice)
 
+        if (priceWithDiscount !== undefined && priceWithDiscount !== '') {
+            if (Number(priceWithDiscount) >= Number(originalPrice)) {
+                errors.priceWithDiscountError = "Price with discount should be less than original price"
+            } else if (+priceWithDiscount < 0) {
+                errors.priceWithDiscountError = "Price with discount shouldn't be negative number"
+            }
+        }
+
+        let inputImage = productImageRef?.current?.files?.[0];
         if (!isUpdateOperation && !inputImage) {
             errors.productImageError = "Image is required"
         }
@@ -84,11 +78,13 @@ export default function ProductForm({ product }: ProductFormProps) {
         let isUpdateOperation = product !== undefined;
 
         let formData = new FormData();
-        formData.append("productName", inputData?.productName);
-        formData.append("originalPrice", inputData?.originalPrice);
-        formData.append("priceWithDiscount", inputData?.priceWithDiscount ? inputData.priceWithDiscount : '');
+        formData.append("productName", productNameRef.current!.value);
+        formData.append("originalPrice", originalPriceRef.current!.value);
+        formData.append("priceWithDiscount", priceWithDiscountRef.current?.value ? priceWithDiscountRef.current!.value : '');
+
+        let inputImage = productImageRef?.current?.files![0];
         if (inputImage) {
-            formData.append("productImage", inputImage, inputImage.name);
+            formData.append("productImage", inputImage,);
         }
 
         if (isUpdateOperation) {
@@ -130,45 +126,30 @@ export default function ProductForm({ product }: ProductFormProps) {
         }
     }
 
-    const changeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setInputData((prevState: any) => ({
-            ...prevState, ...{
-                [event.target.name]: event.target.value
-            }
-        }))
-    }
-
-    const changeImageHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const fileList = event.target.files;
-        if (!fileList) return;
-
-        setInputImage(fileList[0]);
-    };
-
     return (
         <>
             <InfoBadge text={infoBadgeText} class={infoBadgeType} show={showInfoBadge} closeButtonOnClick={() => setShowInfoBadge(false)} />
             <h2 className="text-center">{product ? "Edit product" : "Add new product"}</h2>
             <div className="container pb-5">
-                <form ref={productFormRef}>
+                <form>
                     <div className="mb-2">
                         <label htmlFor="productName" className="form-label">Product name</label>
-                        <input ref={productNameRef} type="text" className="form-control" name="productName" defaultValue={inputData?.productName} onChange={changeInputHandler} />
+                        <input ref={productNameRef} type="text" className="form-control" name="productName" defaultValue={product?.productName} />
                         <small className="text-danger">{inputDataErrors.productNameError}</small>
                     </div>
                     <div className="mb-2">
                         <label htmlFor="originalPrice" className="form-label">Original price</label>
-                        <input ref={originalPriceRef} type="number" step=".01" min="0" className="form-control" name="originalPrice" defaultValue={inputData?.originalPrice} onChange={changeInputHandler} />
+                        <input ref={originalPriceRef} type="number" step=".01" min="0" className="form-control" name="originalPrice" defaultValue={product?.originalPrice} />
                         <small className="text-danger">{inputDataErrors.originalPriceError}</small>
                     </div>
                     <div className="mb-2">
                         <label htmlFor="priceWithDiscount" className="form-label">Price with discount</label>
-                        <input ref={priceWithDiscountRef} type="number" step=".01" min="0" className="form-control" name="priceWithDiscount" defaultValue={inputData?.priceWithDiscount} onChange={changeInputHandler} />
+                        <input ref={priceWithDiscountRef} type="number" step=".01" min="0" className="form-control" name="priceWithDiscount" defaultValue={product?.priceWithDiscount ? product?.priceWithDiscount : ''} />
                         <small className="text-danger">{inputDataErrors.priceWithDiscountError}</small>
                     </div>
                     <div className="mb-2">
                         <label htmlFor="image" className="form-label">Image</label>
-                        <input className="form-control" type="file" name="productImage" onChange={changeImageHandler} />
+                        <input ref={productImageRef} className="form-control" type="file" name="productImage" />
                         <small className="text-danger">{inputDataErrors.productImageError}</small>
                     </div>
                     <div className="text-center">
